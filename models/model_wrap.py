cace3a86bf0e793745f1efa1fe8f4a4ea2a4fe4a
Giussepi <giussepexy@gmail.com>
2025-08-12 13:19:48 +0100

    models/model_wrap.py PointTracker bug fixed, code cleaned

--- models/model_wrap.py
+++ models/model_wrap.py
@@ -15,6 +15,12 @@ from utils.loader import modelLoader
 from utils.losses import extract_patch_from_points, soft_argmax_2d, norm_patches, do_log
 from utils.utils import toNumpy, extract_points, flattenDetection
 
+__all__ = [
+    'labels2Dto3D',
+    'SuperPointFrontend_torch',
+    'PointTracker',
+]
+
 
 def labels2Dto3D(cell_size, labels):
     H, W = labels.shape[0], labels.shape[1]
@@ -32,7 +38,7 @@ def labels2Dto3D(cell_size, labels):
     return labels
 
 
-class SuperPointFrontend_torch(object):
+class SuperPointFrontend_torch:
     """ Wrapper around pytorch net to help with pre and post image processing. """
 
     def __init__(self, config, weights_path, nms_dist, conf_thresh, nn_thresh,
@@ -251,7 +257,7 @@ class SuperPointFrontend_torch(object):
         return pts, pts_desc, dense_desc, heatmap
 
 
-class PointTracker(object):
+class PointTracker:
     """ Class to manage a fixed memory of points and descriptors that enables
     sparse optical flow point tracking.
 
@@ -266,52 +272,52 @@ class PointTracker(object):
         self.maxl = max_length
         self.nn_thresh = nn_thresh
         self.all_pts = []
-        for n in range(self.maxl):
+        for _ in range(self.maxl):
             self.all_pts.append(np.zeros((2, 0)))
         self.last_desc = None
         self.tracks = np.zeros((0, self.maxl + 2))
         self.track_count = 0
         self.max_score = 9999
         self.matches = None
-        self.last_pts = None
+        # self.last_pts = None
         self.mscores = None
 
-    def nn_match_two_way(self, desc1, desc2, nn_thresh):
+    def nn_match_two_way(self, desc1: np.ndarray, desc2: np.ndarray, nn_thresh: float) -> np.ndarray:
         """
         Performs two-way nearest neighbor matching of two sets of descriptors, such
         that the NN match from descriptor A->B must equal the NN match from B->A.
 
-        Inputs:
-          desc1 - MxN numpy matrix of N corresponding M-dimensional descriptors.
-          desc2 - MxN numpy matrix of N corresponding M-dimensional descriptors.
-          nn_thresh - Optional descriptor distance below which is a good match.
+        Kwargs:
+            desc1 <np.ndarray>: MxN matrix of N M-dimensional descriptors.
+            desc2 <np.ndarray>: MxP matrix of P M-dimensional descriptors.
+            nn_thresh  <float>: Maximum descritor distance
 
         Returns:
-          matches - 3xL numpy array, of L matches, where L <= N and each column i is
-                    a match of two descriptors, d_i in image 1 and d_j' in image 2:
-                    [d_i index, d_j' index, match_score]^T
+            matches <np.ndarray>: 3xL matrix, of L matches, where L <= N and each column i is
+                                  a match of two descriptors, idx_i index from desc1 and
+                                  idx_j index from desc2: [idx_i, idx_j, match_score]^T
         """
         assert desc1.shape[0] == desc2.shape[0]
+        assert nn_thresh > 0, nn_thresh
 
         if desc1.shape[1] == 0 or desc2.shape[1] == 0:
             return np.zeros((3, 0))
 
-        if nn_thresh < 0.0:
-            raise ValueError('\'nn_thresh\' should be non-negative')
-
         # Compute L2 distance. Easy since vectors are unit normalized.
-        dmat = np.dot(desc1.T, desc2)
+        dmat = np.dot(desc1.T, desc2)  # NxO
         dmat = np.sqrt(2 - 2 * np.clip(dmat, -1, 1))
         # Get NN indices and scores.
-        idx = np.argmin(dmat, axis=1)
-        scores = dmat[np.arange(dmat.shape[0]), idx]
+        idx = np.argmin(dmat, axis=1)  # N positions selected from desc2
+        scores = dmat[np.arange(dmat.shape[0]), idx]  # N
         # Threshold the NN matches.
-        keep = scores < nn_thresh
+        keep = scores < nn_thresh  # N
         # Check if nearest neighbor goes both directions and keep those.
-        idx2 = np.argmin(dmat, axis=0)
-        keep_bi = np.arange(len(idx)) == idx2[idx]
-        keep = np.logical_and(keep, keep_bi)
-        idx = idx[keep]
+        idx2 = np.argmin(dmat, axis=0)  # O positions selected from desc1
+        keep_bi = np.arange(len(idx)) == idx2[idx]  # FIXME: case min distance links to more than one point
+        keep = np.logical_and(keep, keep_bi)  # N
+        # FIXME: the previous line could be replaced by.
+        # keep = keep * keep_bi
+        idx = idx[keep]  # <= N
         scores = scores[keep]
         # Get the surviving point indices.
         m_idx1 = np.arange(desc1.shape[1])[keep]
@@ -351,23 +357,23 @@ class PointTracker(object):
     def clear_desc(self):
         self.last_desc = None
 
-    def update(self, pts, desc):
+    def update(self, pts: np.ndarray, desc: np.ndarray):
         """ Add a new set of point and descriptor observations to the tracker.
 
-        Inputs
-          pts - 3xN numpy array of 2D point observations.
-          desc - DxN numpy array of corresponding D dimensional descriptors.
+        Kwargs:
+            pts  <np.ndarray>: 3xN matrix of 2D point observations with format [x_i, y_i, confidence_i].
+            desc <np.ndarray>: DxN matrix of D dimensional descriptors corresponding to pts.
         """
-        if pts is None or desc is None:
-            print('PointTracker: Warning, no points were added to tracker.')
-            return
-        # pts = pts.transpose()
-        # desc = desc.transpose()
-        assert pts.shape[1] == desc.shape[1]
-        # assert pts.shape[0] == desc.shape[0]
+        assert isinstance(pts, np.ndarray), type(pts)
+        assert isinstance(desc, np.ndarray), type(desc)
+        assert pts.size > 0
+        assert desc.size > 0
+        assert pts.shape[1] == desc.shape[1], f'pts.shape={pts.shape}, desc.shape={desc.shape}'
+
         # Initialize last_desc.
         if self.last_desc is None:
             self.last_desc = np.zeros((desc.shape[0], 0))
+
         # Remove oldest points, store its size to update ids later.
         remove_size = self.all_pts[0].shape[1]
         self.all_pts.pop(0)
@@ -383,32 +389,40 @@ class PointTracker(object):
         self.tracks = np.hstack((self.tracks, -1 * np.ones((self.tracks.shape[0], 1))))
         # Try to append to existing tracks.
         matched = np.zeros((pts.shape[1])).astype(bool)
-        matches = self.nn_match_two_way(self.last_desc, desc, self.nn_thresh)
-        self.matches = matches
-        pts_id = pts[:2, :]
-        if self.last_pts is not None:
-            id1, id2 = self.last_pts[:, matches[0, :].astype(int)], pts_id[:, matches[1, :].astype(int)]
+        self.matches = self.nn_match_two_way(self.last_desc, desc, self.nn_thresh)
+        # pts_id = pts[:2, :]  # [x_i, y_i]
 
-            self.matches = np.concatenate((id1, id2), axis=0)
-        for match in matches.T:
+        # NOTE: this is wrong because the subsequent for loop employs offsets to update the indexes,
+        #       and the following lines replaces the 3xL indexes matrix with a 4xL points matrix.
+        #       As a result, keeping the following lines will make the algorithm to wrongly update points
+        #       with index offsets.
+        # if self.last_pts is not None:
+        #     id1 = self.last_pts[:, self.matches[0, :].astype(int)]  # matched previous pts x_i,y_i [2,R]
+        #     id2 = pts_id[:, self.matches[1, :].astype(int)]  # matched new pts x_i,y_i  [2,R]
+        #     self.matches = np.concatenate((id1, id2), axis=0)  # [4,R]
+        # FIXME: Commenting the above lines makes get_matches and get_scores return the same matrix.
+        #        This change requires modification in the evaluation.py script
+
+        for match_ in self.matches.T:
             # Add a new point to it's matched track.
-            id1 = int(match[0]) + offsets[-2]
-            id2 = int(match[1]) + offsets[-1]
+            id1 = match_[0] + offsets[-2]  # updated old descriptor idx
+            id2 = match_[1] + offsets[-1]  # updated new descriptor idx
             found = np.argwhere(self.tracks[:, -2] == id1)
             if found.shape[0] > 0:
-                matched[int(match[1])] = True
+                matched[int(match_[1])] = True
                 row = int(found)
-                self.tracks[row, -1] = id2
+                self.tracks[row, -1] = id2  # adding new descriptor idx as the latest point in the row
                 if self.tracks[row, 1] == self.max_score:
                     # Initialize track score.
-                    self.tracks[row, 1] = match[2]
+                    self.tracks[row, 1] = match_[2]
                 else:
                     # Update track score with running average.
                     # NOTE(dd): this running average can contain scores from old matches
                     #           not contained in last max_length track points.
                     track_len = (self.tracks[row, 2:] != -1).sum() - 1.
                     frac = 1. / float(track_len)
-                    self.tracks[row, 1] = (1. - frac) * self.tracks[row, 1] + frac * match[2]
+                    self.tracks[row, 1] = (1. - frac) * self.tracks[row, 1] + frac * match_[2]
+
         # Add unmatched tracks.
         new_ids = np.arange(pts.shape[1]) + offsets[-1]
         new_ids = new_ids[~matched]
@@ -425,23 +439,4 @@ class PointTracker(object):
         self.tracks = self.tracks[keep_rows, :]
         # Store the last descriptors.
         self.last_desc = desc.copy()
-        self.last_pts = pts[:2, :].copy()
-
-    def get_tracks(self, min_length):
-        """ Retrieve point tracks of a given minimum length.
-        Input
-          min_length - integer >= 1 with minimum track length
-        Output
-          returned_tracks - M x (2+L) sized matrix storing track indices, where
-            M is the number of tracks and L is the maximum track length.
-        """
-        if min_length < 1:
-            raise ValueError('\'min_length\' too small.')
-        valid = np.ones((self.tracks.shape[0])).astype(bool)
-        good_len = np.sum(self.tracks[:, 2:] != -1, axis=1) >= min_length
-        # Remove tracks which do not have an observation in most recent frame.
-        not_headless = (self.tracks[:, -1] != -1)
-        keepers = np.logical_and.reduce((valid, good_len, not_headless))
-        returned_tracks = self.tracks[keepers, :].copy()
-
-        return returned_tracks
+        # self.last_pts = pts[:2, :].copy()  # [x_i, y_i]
